8642cf4b98a81f84ea4daf679fc26ffe532ab631
Jason Rohrer <jasonrohrer@fastmail.fm>
2019-07-15 21:16:57 -0700

    Added scripts for running client and editor, along with notes about clearing emote settings.

---
+++ scripts/runClient.bat
@@ -0,0 +1,22 @@
+echo off
+
+echo Starting Client...
+
+
+echo.
+echo.
+
+cd StarFacerData
+
+
+OneLife
+
+echo.
+echo.
+
+echo Client process finished.
+
+echo.
+echo.
+
+cd ..
---
+++ scripts/runEditor.bat
@@ -0,0 +1,22 @@
+echo off
+
+echo Starting Editor...
+
+
+echo.
+echo.
+
+cd StarFacerData
+
+
+EditOneLife
+
+echo.
+echo.
+
+echo Client process finished.
+
+echo.
+echo.
+
+cd ..
